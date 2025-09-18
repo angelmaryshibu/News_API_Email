4aac486f55a82b32b9899422363c90e8c8de312a
Angel Mary Shibu <angelmaryash2@gmail.com>
2025-09-18 08:51:04 +0530

    Added URL, Limit News , Language control

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
+import send_email
+
+topic="tesla"
 
 api_key="bbdca220f81a4c1bb0e90d00830c9cf4"
 
-url="https://newsapi.org/v2/everything?q=tesla&from=2025-08-17&" \
-    "sortBy=publishedAt&apiKey=bbdca220f81a4c1bb0e90d00830c9cf4"
+url="https://newsapi.org/v2/everything?" \
+    f"q={topic}&" \
+    "from=2025-08-17&" \
+    "sortBy=publishedAt&apiKey=bbdca220f81a4c1bb0e90d00830c9cf4&" \
+    "languane=en"
 
 # Make request
 request=(requests.get(url))
@@ -11,8 +17,19 @@ request=(requests.get(url))
 # Get a dictionary with data
 content = request.json()
 
+message=""
+
 # Access the articles titles and discription
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+for article in content["articles"][:20]:
+    if article["title"] is not None and article["description"] is not None:
+        message= "Subject:Today's news"+ "\n" + message + article["title"] + "\n" \
+            + article["description"] \
+            + "\n"+ article["url"] + "\n\n"
+
+message=message.encode("utf-8")
+
+# Send the email
+send_email.send_email(message)
+
+
     
